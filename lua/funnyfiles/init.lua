e3f39b3b70bf20b424aaacc0e6c253802b5eb600
aikooo7 <prozinhopro1973@gmail.com>
2023-11-12 12:10:35 +0000

    feat: add DeleteFile function

--- lua/funnyfiles/init.lua
+++ lua/funnyfiles/init.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.CreateFile()
   local file = vim.fn.input("File name: ")
 
+
   vim.loop.fs_open(file, "w", 438, function (err)
     if err then
       error("Error creating file " .. file " with error: " .. err)
@@ -12,8 +13,22 @@ function M.CreateFile()
   end)
 end
 
+function M.DeleteFile()
+  local file = vim.fn.input("File name: ")
+
+  vim.loop.fs_unlink(file, function (err)
+    if err then
+      error("Error deleting file " .. file .. " with error: " .. err)
+    else
+      print("Successully deleted file " .. file)
+    end
+  end)
+
+end
+
 end
 
 vim.api.nvim_create_user_command("CreateFile", M.CreateFile, {})
+vim.api.nvim_create_user_command("DeleteFile", M.DeleteFile, {})
 
 return M
