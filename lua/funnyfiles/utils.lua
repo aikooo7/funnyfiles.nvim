ed909c867a036365a545643c64078afc7181c0ec
aikooo7 <prozinhopro1973@gmail.com>
2023-12-02 17:48:39 +0000

    feat(lua): checks if user give input and if not warns the user and asks input again until user gives something

--- lua/funnyfiles/utils.lua
+++ lua/funnyfiles/utils.lua
@@ -1,7 +1,13 @@
 local M = {}
 
 function M.input(message)
+	repeat
 		local user_input = vim.fn.input(message)
+		if user_input ~= "" then
+			print("Input should not be empty")
+		end
+
+	until user_input ~= ""
 
 	return user_input
 end
